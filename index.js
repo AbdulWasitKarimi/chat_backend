db09578f58a4258f7ac294fb434750b6e0a8ef5f
Wasit Karimi <wasitkarimi0@gmail.com>
2023-09-27 15:00:04 +0430

    =init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const userRoute = require("./Routes/userRoutes")
+
+const app = express()
+require("dotenv").config()
+
+app.use(express.json())
+app.use(cors())
+app.use("/api/users", userRoute)
+app.get("/", (req, res) => {
+    res.send("Welcome to our OLTS apis")
+})
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+    console.log(`Server runnig on port : ${port}`);
+})
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('mongodb connection established')).catch((error) => console.log('mongodb connection failed: ', error.message))
